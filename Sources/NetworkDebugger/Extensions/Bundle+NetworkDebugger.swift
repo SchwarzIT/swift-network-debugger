381e62628381ecffcd2d81bff1f043f4490a2153
Michael Artes <michael.artes@mail.schwarz>
2023-04-26 20:53:35 +0200

    Added compatability for pods bundle

---
+++ Sources/NetworkDebugger/Extensions/Bundle+NetworkDebugger.swift
@@ -0,0 +1,57 @@
+//
+//  Bundle+NetworkDebugger.swift
+//
+//
+//  Created by Michael Artes on 26.04.23.
+//  Copyright © 2023 Schwarz IT KG.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+extension Bundle {
+    static let networkDebugger: Bundle = {
+        let bundleName = "NetworkDebugger_NetworkDebugger"
+        
+        let overrides: [URL]
+        #if DEBUG
+        if let override = ProcessInfo.processInfo.environment["PACKAGE_RESOURCE_BUNDLE_URL"] {
+            overrides = [URL(fileURLWithPath: override)]
+        } else {
+            overrides = []
+        }
+        #else
+        overrides = []
+        #endif
+        
+        let candidates = overrides + [
+            // Bundle should be present here when the package is linked into an App.
+            Bundle.main.resourceURL,
+            
+            // Bundle should be present here when the package is linked into a framework.
+            Bundle(for: NetworkDebugger.self).resourceURL,
+            
+            // For command-line tools.
+            Bundle.main.bundleURL,
+        ]
+        
+        for candidate in candidates {
+            let bundlePath = candidate?.appendingPathComponent(bundleName + ".bundle")
+            if let bundle = bundlePath.flatMap(Bundle.init(url:)) {
+                return bundle
+            }
+        }
+        fatalError("unable to find bundle named NetworkDebugger_NetworkDebugger")
+    }()
+}
